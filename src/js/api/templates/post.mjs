1dba8c82d57f167b93a289a71ffb9475c4772f3a
S10ANDK <91675991+S10ANDK@users.noreply.github.com>
2022-10-15 22:48:29 +0200

    UI enhancement for single post on mobile view

--- src/js/api/templates/post.mjs
+++ src/js/api/templates/post.mjs
@@ -47,7 +47,7 @@ export function postTemplate(postData) {
 
 export function postTemplateSingle(postData) {
     const post = document.createElement("div");
-    post.classList.add("post", "row", "w-75", "mx-auto", "my-5", "border", "rounded-3", "p-5"); 
+    post.classList.add("post", "row", "w-100", "mx-auto", "my-5", "border", "rounded-3", "p-3"); 
 
     const postHeading = document.createElement("h2");
     postHeading.classList.add("postHeading", "col");
@@ -56,7 +56,7 @@ export function postTemplateSingle(postData) {
     post.appendChild(postHeading);
 
     const createdDateP = document.createElement("p");
-    createdDateP.classList.add("createdDate", "col-4", "text-end");
+    createdDateP.classList.add("createdDate", "col-12", "col-sm-4", "text-end");
     const createdDate = document.createTextNode(new Date(postData.created).toGMTString());
     createdDateP.appendChild(createdDate);
     post.appendChild(createdDateP);
