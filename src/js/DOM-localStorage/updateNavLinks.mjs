a191207e8c708ba68631e6a2e6c7e288036e9d43
S10ANDK <91675991+S10ANDK@users.noreply.github.com>
2022-10-15 23:07:15 +0200

    UX tweak

--- src/js/DOM-localStorage/updateNavLinks.mjs
+++ src/js/DOM-localStorage/updateNavLinks.mjs
@@ -7,14 +7,17 @@ export function updateNavLinks () {
     const storedUsername = localStorage.getItem("name");
     const loginLink = document.querySelector("#loginLink");
     const registerLink = document.querySelector("#registerLink");
+    const feedLink = document.querySelector("#feedLink");
     const logo = document.querySelector("#logo");
 
     if (localStorage.getItem("name") === null) {
         usernameDisplayed.style.display = "none";
+        feedLink.style.display = "none";
     } else {
         usernameDisplayed.innerHTML = storedUsername;
         loginLink.style.display = "none";
         registerLink.style.display = "none";
+        feedLink.style.display = "block";
         logo.href = "";
         logo.href = "/html/posts";
     }
